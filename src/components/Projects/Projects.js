63895403f5d8c12948aa43d2f78eb292f4461eb1
Rajesh <rajeshjoshi9865@gmail.com>
2023-01-10 11:41:57 +0530

    first commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import suicide from "../../Assets/Projects/suicide.png";
+import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Banking system"
+              description="It is just a demo for a banking system."
+              ghLink="https://github.com/AnujKandel9880/project"
+              
+            />
+          </Col>
+
+          
+
+          
+
+          
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
